a032b12547def60577401fcd69bd33a184caa2d6
joshtsar <122890377+joshtsar@users.noreply.github.com>
2023-07-16 22:12:34 +1000

    Delete GitHubUsersCaptialTransportByJiahuaTong directory

--- GitHubUsersCaptialTransportByJiahuaTong/Service/Interfaces/IGHPublicApi.cs
+++
@@ -1,9 +0,0 @@
-﻿using GitHubUsersCaptialTransportByJiahuaTong.DTOs;
-
-namespace GitHubUsersCaptialTransportByJiahuaTong.Service.Interfaces
-{
-    public interface IGHPublicApi
-    {
-        Task<IEnumerable<GithubUserInfo>> GetUserInfoByUserNames(List<string> userNames);
-    }
-}
