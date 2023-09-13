b23c5a2e13547e310b3f3c8a1f0881fcb72c4d7a
joshtsar <122890377+joshtsar@users.noreply.github.com>
2023-09-13 20:19:09 +1000

    Delete Github Users Info Retrieve web API DEMO Project directory

--- Github Users Info Retrieve web API DEMO Project/Service/Interfaces/IGHPublicApi.cs
+++
@@ -1,9 +0,0 @@
-﻿using GitHubUsersInfoDemoByJiahuaTong.DTOs;
-
-namespace GitHubUsersInfoDemoByJiahuaTong.Service.Interfaces
-{
-    public interface IGHPublicApi
-    {
-        Task<IEnumerable<GithubUserInfo>?> GetUserInfoByUserNames(List<string> userNames);
-    }
-}
