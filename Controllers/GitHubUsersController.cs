f3ee05bc1a6e48db6b1fdd82bb07460a91b6dffd
joshtsar <122890377+joshtsar@users.noreply.github.com>
2023-09-13 20:23:38 +1000

    Add files via upload

---
+++ Controllers/GitHubUsersController.cs
@@ -0,0 +1,33 @@
+﻿using GitHubUsersInfoDemoByJiahuaTong.DTOs;
+using GitHubUsersInfoDemoByJiahuaTong.Service.Interfaces;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace GitHubUsersInfoDemoByJiahuaTong.Controllers
+{
+    [ApiController]
+    [Route("api/githubusersinfo")]
+    public class GitHubUsersController : ControllerBase
+    {
+        private readonly IGHPublicApi _githubPublicApiService;
+        public GitHubUsersController(IGHPublicApi githubPublicApi)
+        {
+            _githubPublicApiService = githubPublicApi ?? throw new ArgumentNullException(nameof(githubPublicApi));
+        }
+
+        // GET: GitHubUsersController
+        [HttpGet("acquireUsersInfo")]
+        public async Task<ActionResult<IEnumerable<GithubUserInfo>>?> RetrieveUsers([FromQuery] List<string> UserNameList)
+        {
+            var result = await _githubPublicApiService.GetUserInfoByUserNamesAsync(UserNameList);
+            
+            if (Response.StatusCode == 200&& result?.Count() > 0)
+                return Ok(result);
+            else
+            {
+                //await Response.Body.FlushAsync();
+                return null;
+            }
+        }
+    }
+}
---
+++ Service/Interfaces/IGHPublicApi.cs
@@ -0,0 +1,9 @@
+﻿using GitHubUsersInfoDemoByJiahuaTong.DTOs;
+
+namespace GitHubUsersInfoDemoByJiahuaTong.Service.Interfaces
+{
+    public interface IGHPublicApi
+    {
+        Task<IEnumerable<GithubUserInfo>?> GetUserInfoByUserNamesAsync(List<string> userNames);
+    }
+}
